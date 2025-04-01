bd2502e179639a48e19bc16fd57144419c918d52
raneyf <anton.kinstler.04@gmail.com>
2025-04-01 20:08:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+var massiveText=[
+  'Здравствуйте, гость'
+  'Привет, гость'
+  'Добрый день, гость'
+];
+var randomNum=Math.floor(Math.random()*3)
+alert(massiveText[randomNum]);
+
 var count = 0;
+var upgrade0=1
 var button = document.getElementById("click-me");
 var counter = document.getElementById("counter");
-button.addEventListener("click", function() {
+upgrade1Button.addEventListener('click', function()P{})
+  upgrade0=upgrade0+1;
+  
+button.addEventListener("click", function(){
   count++;
   counter.innerHTML = count;
 });
