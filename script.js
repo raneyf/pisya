17992e2b06ff5b88cde2ca001df9858eeffcb209
raneyf <anton.kinstler.04@gmail.com>
2025-04-07 00:45:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var h1Id=document.getElementyById('h1');
 var count = 0;
 var button = document.getElementById("click-me");
 var counter = document.getElementById("counter");
-button.addEventListener("click", function() {
+button.addEventListener('click', function(){
   count++;
   counter.innerHTML = count;
 });
