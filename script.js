df7a7a955ec4aeb4a4f747340691bfc3a072e933
raneyf <anton.kinstler.04@gmail.com>
2025-04-11 20:21:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var massiveText=[
+    'Здравствуйте.',
+    'привет.',
+    'добрый день.'
+];
+var randomNum=Math.floor(Math.random()*3);
+alert(massiveText[randomNum]);
+
 var count = 0;
 var button = document.getElementById("click-me");
 var counter = document.getElementById("counter");
