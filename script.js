e80eb774bb70377b5ea8ab0ab1f3c1b3707b4644
raneyf <anton.kinstler.04@gmail.com>
2025-04-13 17:30:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var massiveText=[
-    'Здравствуйте.',
-    'привет.',
-    'добрый день.'
-];
-var randomNum=Math.floor(Math.random()*3);
-alert(massiveText[randomNum]);
-
-var count = 0;
+var shop=document.getElementById('openShop');
+var back=document.getElementById('back');
 var button = document.getElementById("click-me");
+var upgrade1Button=document.getElementById('upgrade1');
+var count = 0;
+var upgrade0=0;
 var counter = document.getElementById("counter");
+
+shop.addEventListener('click',function(){
+  shop.classList.add('hidden');
+  back.classList.remove('hidden');
+  upgrade1Button.classList.remove("hidden");
+});
+back.addEventListener('click',function(){
+  shop.classList.remove('hidden');
+  back.classList.add('hidden');
+  upgrade1Button.classList.add('hidden');
+});
+upgrade1Button.addEventListener('click',function(){
+  count=count-10;
+  upgrade0=upgrade0+1;
+  counter.innerHTML = count;
+});
 button.addEventListener('click', function(){
   count++;
+  count=count+upgrade0;
   counter.innerHTML = count;
 });
